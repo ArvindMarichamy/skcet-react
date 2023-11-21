3c17ebac546cb623cc8d37e1e0b88ba3a5b10aa6
ArvindMarichamy <151006520+ArvindMarichamy@users.noreply.github.com>
2023-11-21 11:08:20 +0530

    Add files via upload

---
+++ src/components/Login.js
@@ -0,0 +1,41 @@
+import React, { Component } from 'react';
+import "./Login.css";
+import { Link } from 'react-router-dom';
+class Login extends Component {
+  render() {
+    return (
+      <div className="login">
+        <h4>LOGIN</h4>
+        <form>
+          <div className="text_area">
+            <input
+              type="text"
+              placeholder='Enter Username'
+              className="text_input"
+            />
+          </div>
+          <div className="text_area">
+            <input
+              type="password"
+              placeholder='Enter Password'
+              className="text_input"
+            />
+          </div>
+          <Link to="/"><input
+            type="submit"
+            value="Submit"
+            className="btn"
+          /></Link>
+        </form>
+        <div className='flex'>
+        <center>
+          <span>Dont't have an account?</span>
+        <a className="link" href="/sign up">Register Now</a>
+        </center>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Login;
